179d90a5498a34e127d5e268c762e48423ee4350
JONENY <jumobile4091@gmail.com>
2025-10-28 21:48:42 +0700

    Delete apiCarRental.js

--- apiCarRental.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const carRentalController = require('../controllers/CarRentalController');
-const apiCarRentalRouter = express.Router();
-
-apiCarRentalRouter.get('/carrental', carRentalController.handleGetCar);
-
-module.exports = apiCarRentalRouter;
